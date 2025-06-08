4b6d72cfd01fa51356fa70d6796809ed993948a6
berlinsk <berliniumyar@gmail.com>
2025-06-08 13:00:11 +0300

    Initial Commit

---
+++ GameBacklogApp/GameBacklogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameBacklogAppApp.swift
+//  GameBacklogApp
+//
+//  Created by Берлинский Ярослав Владленович on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct GameBacklogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
